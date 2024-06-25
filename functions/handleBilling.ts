7e15d7062ddb3bc913e83900730004e690e2cbb5
Normal Gaussian <gitlab.march.2019@normal-gaussian.com>
2024-06-25 17:04:35 +0100

    Adds function stubs

---
+++ functions/handleBilling.ts
@@ -0,0 +1,7 @@
+import { HandleBilling } from '@teamkeel/sdk';
+
+// To learn more about what you can do with custom functions, visit https://docs.keel.so/functions
+export default HandleBilling(async (ctx, inputs) => {
+    // TODO: implement
+    throw new Error('Not implemented');
+});
